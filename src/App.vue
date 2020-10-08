6555d2fe94903b1ed19559c169a3421ee2aa4639
shawonrog <shahriar15-1269@diu.edu.bd>
2020-10-08 13:06:21 +0600

    custome-event

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
+ <!-- This is The Master Component -->
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>{{ title }} </h2>
+    <Navbar></Navbar>
+    <AllFriends :friends="friends" @delete="deleteFriend">
+    </AllFriends>
+    <OnlineFriends :friends="friends"></OnlineFriends>
   </div>
 </template>
-
+ 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    import Navbar from './components/Navbar.vue'
+    import AllFriends from './components/AllFriends.vue'
+    import OnlineFriends from './components/OnlineFriends.vue'
+    export default {
+      name: 'App',
+      data() {
+        return {
+          title: 'Congratz, Our Vue Website is Working Fine With VUE.JS!',
+          friends:[
+            {   name:'Shawon',    online:true     },
+            {   name:'Shahriar',  online:false    },
+            {   name:'Jhon',      online:true     },
+            {   name:'David',     online:false    },
+            {   name:'Miller',    online:true     },
+            {   name:'Watson',    online:true     }
+          ]
+        }
+      },
+      components:{
+        Navbar,
+        AllFriends,
+        OnlineFriends
+      },
+      methods: {
+        deleteFriend(payload){
+          // console.log(payload);
+          this.friends = this.friends.filter(friend =>{
+            return friend.name !== payload.name;
+          })
+        }
+      },
+    
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li><a href="">HOME</a></li>
+        <li><a href="">POST</a></li>
+        <li><a href="">ABOUT</a></li>
+        <li><a href="">CONTACT</a></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  },
+};
+</script>
+ 
+<style scoped>
+nav {
+  text-align: center;
+}
+nav ul {
+  padding: 0;
+}
+nav li {
+  display: inline-block;
+  list-style: none;
+  margin: 0;
+  padding: 8px;
+}
+</style>
