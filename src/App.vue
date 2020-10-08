82fc17bd6ea6b7b7e91577175068f37a4b2d48db
shawonrog <shahriar15-1269@diu.edu.bd>
2020-10-08 13:26:01 +0600

    lifecycle hook

--- src/App.vue
+++ src/App.vue
@@ -4,43 +4,26 @@
   <div id="app">
     <h2>{{ title }} </h2>
     <Navbar></Navbar>
-    <AllFriends :friends="friends" @delete="deleteFriend">
-    </AllFriends>
-    <OnlineFriends :friends="friends"></OnlineFriends>
+    <Hook></Hook>
   </div>
 </template>
  
 <script>
     import Navbar from './components/Navbar.vue'
-    import AllFriends from './components/AllFriends.vue'
-    import OnlineFriends from './components/OnlineFriends.vue'
+    import Hook from './components/Hook.vue'
+
     export default {
       name: 'App',
       data() {
         return {
           title: 'Congratz, Our Vue Website is Working Fine With VUE.JS!',
-          friends:[
-            {   name:'Shawon',    online:true     },
-            {   name:'Shahriar',  online:false    },
-            {   name:'Jhon',      online:true     },
-            {   name:'David',     online:false    },
-            {   name:'Miller',    online:true     },
-            {   name:'Watson',    online:true     }
-          ]
         }
       },
       components:{
         Navbar,
-        AllFriends,
-        OnlineFriends
+        Hook
       },
       methods: {
-        deleteFriend(payload){
-          // console.log(payload);
-          this.friends = this.friends.filter(friend =>{
-            return friend.name !== payload.name;
-          })
-        }
       },
     
   }
---
+++ src/components/Hook.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="Hook">
+        <h1>{{titile}}</h1>
+        <button @click="changeTitle">Change Title</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Hook",
+  data() {
+    return {
+        titile: 'Hook Data'
+    };
+  },
+  methods: {
+    changeTitle(){
+            this.titile = 'Title Changed'
+    }
+  },
+  beforeCreate() {
+      
+  },
+  created() {
+  },
+  beforeUpdate() {
+      alert('Before UPDATE TITLE')
+  },
+  
+};
+</script>
+ 
+<style scoped>
+    
+</style>
